bfbdc18d5d0ace2fd7260c24bfe4db67277c0a40
Henok-Haile <henokhaile53@gmail.com>
2024-10-04 11:48:03 +0300

    assignment 2

---
+++ JavaScript/labs/lab2.js
@@ -0,0 +1,57 @@
+var randomNumber = Math.floor(Math.random()*10);
+console.log(randomNumber);
+
+// var mydate = new Date();
+// console.log('the date is: ' + mydate.getDate() + '-' + (mydate.getMonth()+1) + '-' + mydate.getFullYear())
+
+
+
+// var x='5.4'
+// var y=3
+
+// console.log(parseInt(x)+y);
+
+
+// var str = 'welcome to javascript course';
+
+// console.log(str.indexOf('to'));
+// console.log(str.startsWith('welcome'))
+
+// console.log(str.trim())
+
+// console.log(str.length);
+// console.log(str.slice(7));
+// console.log(str.replace('javascript', 'fullstack'));
+// console.log(str.toUpperCase());
+
+
+
+// var x = 5;
+// var y = 3;
+
+// // function
+// function addTwoNumbers(num1, num2){
+//     return num1+num2;
+// }
+// console.log(addTwoNumbers(5,2));
+
+
+// comparison
+
+// console.log(x <= y)
+
+// logical
+
+// console.log(x>5 || y<3)
+
+// //Assignment
+// console.log(x += 2);
+// console.log(y *= 2);
+
+// // Arthimetic
+// console.log(x + y);
+// console.log(x - y);
+// console.log((x / y).toFixed(1))
+// console.log(x ** 3);
+// console.log(++x);
+// console.log(--y);
