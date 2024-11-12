65e60eb1cde937d4f3d00a575d15681a83749a4f
Henok-Haile <henokhaile53@gmail.com>
2024-11-12 13:26:50 +0300

    Task 9

--- JavaScript/assignments/assignment9/4.js
+++ JavaScript/assignments/assignment9/4.js
@@ -1,4 +1,24 @@
-fetchData()
-  .then(data => processData(data))
-  .then(result => console.log("Processed result:", result))
-  .catch(error => console.error("An error occurred:", error));
+
+function fetchData() {
+    return new Promise(async (resolve) => {
+      await delay(2000); // Wait for 2 seconds
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      resolve(randomNumber);
+    });
+  }
+  
+  
+  function processData(data) {
+    return new Promise(async (resolve) => {
+      console.log("Processing data:", data);
+      await delay(1000);
+      resolve(data * 3);
+    });
+  }
+  
+  
+  fetchData()
+    .then(data => processData(data))
+    .then(result => console.log("Processed result:", result))
+    .catch(error => console.error("An error occurred:", error));
+  
