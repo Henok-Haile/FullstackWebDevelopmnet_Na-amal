1af3e0809a4c0d77247423118ab1f3bf59e6a79b
Henok-Haile <henokhaile53@gmail.com>
2024-11-12 13:24:34 +0300

    Task 9

--- JavaScript/assignments/assignment9/chaineed.js
+++ JavaScript/assignments/assignment9/chaineed.js
@@ -1,27 +1,22 @@
-// 1. delay function
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-  // 2. fetchData function
+
   function fetchData() {
     return new Promise(async (resolve) => {
       await delay(2000); // Wait for 2 seconds
-      const randomNumber = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
       resolve(randomNumber);
     });
   }
   
-  // 3. processData function
+  
   function processData(data) {
     return new Promise(async (resolve) => {
-      console.log("Processing data:", data); // Log the data
-      await delay(1000); // Wait for 1 second
-      resolve(data * 2); // Resolve with data multiplied by 2
+      console.log("Processing data:", data);
+      await delay(1000);
+      resolve(data * 3);
     });
   }
   
-  // 4. Chain the functions together
+  
   fetchData()
     .then(data => processData(data))
     .then(result => console.log("Processed result:", result))
